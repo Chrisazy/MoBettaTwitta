ac415723f782fdd78218eea455776bcc8790df4f
unknown <wirtc@WIRTC2.americas.hpqcorp.net>
2015-03-07 13:55:30 -0500

    myql connection

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
 <?php
 if(!isset($_GET['id'])) {
-
+	$host = "us-cdbr-azure-east2-d.cloudapp.net";
+    $user = "b34779233e0057";
+    $pwd = "2f7abdff";
+    $db = "MoBettaAZO1IzF59;";
+    // Connect to database.
+    try {
+        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch(Exception $e){
+        die(var_dump($e));
+    }
 } else {
-	// We have an id!
-	if(isset($_REQUEST['json'])) {
-		// Don't just display it, noob.
+	$text = getTweet($_GET['id']);
+	if(isset($_REQUEST['raw'])) {
+		// Encode in a JSON Object and print that
 	} else {
 		// They just cicked the link...Dang.
 		echo "Well, here's the link for ".$_GET['id'];
 		echo "<br>";
 	}
 }
-
-
 ?>
